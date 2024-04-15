5cb6f295a4f2b24e224e488863179e855d46030f
Mridul Gupta <mridul.gupta@gmail.com>
2024-04-16 09:48:31 +1000

    typealias instead of copy (expose all devices)

--- nitrogen/src/main/kotlin/tech/sarahgallitz/nitrogen/environment/DeviceQualifiers.kt
+++ nitrogen/src/main/kotlin/tech/sarahgallitz/nitrogen/environment/DeviceQualifiers.kt
@@ -2,23 +2,4 @@ package tech.sarahgallitz.nitrogen.environment
 
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 
-object DeviceQualifiers {
-    const val ResizableExperimental = RobolectricDeviceQualifiers.ResizableExperimental
-    const val SmallPhone = RobolectricDeviceQualifiers.SmallPhone
-    const val MediumPhone = RobolectricDeviceQualifiers.MediumPhone
-    const val MediumTablet = RobolectricDeviceQualifiers.MediumTablet
-    const val NexusOne = RobolectricDeviceQualifiers.NexusOne
-    const val Nexus7 = RobolectricDeviceQualifiers.Nexus7
-    const val Nexus9 = RobolectricDeviceQualifiers.Nexus9
-    const val PixelC = RobolectricDeviceQualifiers.PixelC
-    const val PixelXL = RobolectricDeviceQualifiers.PixelXL
-    const val Pixel4 = RobolectricDeviceQualifiers.Pixel4
-    const val Pixel4XL = RobolectricDeviceQualifiers.Pixel4XL
-    const val Pixel4a = RobolectricDeviceQualifiers.Pixel4a
-    const val Pixel5 = RobolectricDeviceQualifiers.Pixel5
-    const val Pixel6 = RobolectricDeviceQualifiers.Pixel6
-    const val Pixel6Pro = RobolectricDeviceQualifiers.Pixel6Pro
-    const val Pixel6a = RobolectricDeviceQualifiers.Pixel6a
-    const val Pixel7Pro = RobolectricDeviceQualifiers.Pixel7Pro
-    const val Pixel7 = RobolectricDeviceQualifiers.Pixel7
-}
+typealias DeviceQualifiers = RobolectricDeviceQualifiers
